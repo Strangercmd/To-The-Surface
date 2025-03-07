221e0382e2db702ecc203197c214050e1f443afc
Strangercmd <akhiwakde@gmail.com>
2025-03-07 14:15:20 -0700

    modified:   .editorconfig 	modified:   .gitattributes 	modified:   .gitignore 	modified:   character_body_2d.gd 	modified:   mai4967.tmp 	modified:   main_menu.gd 	modified:   main_temp.tscn 	modified:   project.godot

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -4,6 +4,7 @@ const SPEED = 200.0
 const JUMP_VELOCITY = -245.0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -13,8 +14,8 @@ func _physics_process(delta: float) -> void:
 	# Handle jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	
-	
+
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
@@ -27,13 +28,13 @@ func _physics_process(delta: float) -> void:
 		animated_sprite_2d.animation = "running"
 	else:
 		animated_sprite_2d.animation = "idle"
-	
-	
+
+
 	move_and_slide()
 
 	var isLeft = velocity.x < 0
 	animated_sprite_2d.flip_h = isLeft
-	
+
 	if Input.is_action_just_pressed("exit"):
 		get_tree().quit()
-	
+
